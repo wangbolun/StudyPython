878659dbd10c6e899cecaf8977255f56e5995c6b
wangbolun <540453724@qq.com>
2019-05-24 09:14:45 +0800

    变量+常见的数据类型

---
+++ class/class_3.py
@@ -0,0 +1,20 @@
+#变量x=1 y=2
+#一旦创建了变量 然后赋值 就会存在python内存
+# a =1
+# print(a)
+#NameError: name 'a' is not defined;没有定义a
+
+
+#常见的数据类型
+#数字、字符串、元祖、列表、字典
+#数字：整数、浮点数
+
+#常用的数字数据：inf float 关键词
+
+#type() 可以判断数据的类型
+a=1
+print(type(a))
+
+b = 2.009
+print(type(b))
+
