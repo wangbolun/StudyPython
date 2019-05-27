d36a88c5e43a31ddb1e25bcae55079832f31ea19
wangbolun <540453724@qq.com>
2019-05-27 22:07:03 +0800

    增加class_5

---
+++ class/class_5.py
@@ -0,0 +1,12 @@
+#字符串切片
+a = 'lemon'
+#字符串切片：字符串名[m:n:k]
+#m索引开始的地方 n索引结束的地方 k步长
+print(a[0:4:2]) #k=024
+print(a[2:5:1])
+print(a[:])#从头取到尾
+
+str_1 = 'hello lemon class'
+#在字符串 空格也是一个字符
+print(str_1[2:17:2])
+
