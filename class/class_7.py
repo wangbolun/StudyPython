6ebaab5c4cca5d3dfdf85bfd07324045f673443e
wangbolun <540453724@qq.com>
2019-06-04 11:50:34 +0800

    新增class_6

---
+++ class/class_7.py
@@ -0,0 +1,13 @@
+#字符串内建函数
+str_1='hello'
+#大小写切换upper() lower()
+res=str_1.upper()
+str_2='PYTHON'
+print('转换后的结果{}'.format(res))
+
+res=str_2.lower()
+print('转换后的结果{}'.format(res))
+
+#字符串的查找 find()
+
+res =str_1.find(0)
